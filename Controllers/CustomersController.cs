20e81e8c94c6424778fba94d090b43529b942a36
Aaron Bednikof <beday003@mymail.unisa.edu.au>
2025-08-17 17:24:39 +0930

    Implemented customer and partial suburb search

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,198 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AOWebApp.Data;
+using AOWebApp.Models;
+using AOWebApp.ViewModels;
+
+namespace AOWebApp.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly AmazonDbContext _context;
+
+        public CustomersController(AmazonDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Customers (VB approach)
+        public async Task<IActionResult> Index(string searchText, string selectedSuburb)
+        {
+            // Get distinct suburbs for dropdown (from Address table)
+            var suburbs = await _context.Addresses
+                .Where(a => !string.IsNullOrEmpty(a.Suburb))
+                .Select(a => a.Suburb)
+                .Distinct()
+                .OrderBy(s => s)
+                .ToListAsync();
+
+            // pass data to view via ViewBag
+            ViewBag.Suburbs = suburbs;
+            ViewBag.SearchText = searchText;
+            ViewBag.SelectedSuburb = selectedSuburb;
+
+            // empty list - no records shown initially
+            List<Customer> customers = new List<Customer>();
+
+            // only search and populate results if there's search criteria provided
+            if (!string.IsNullOrWhiteSpace(searchText))
+            {
+                var customersQuery = _context.Customers
+                    .Include(c => c.Address)
+                    .AsQueryable();
+
+                customers = await customersQuery
+                    .OrderBy(c => !(c.FirstName.Contains(searchText))).ThenBy(c => c.FirstName)
+                    .ThenBy(c => c.LastName)
+                    .ToListAsync();
+
+                if (!string.IsNullOrWhiteSpace(selectedSuburb))
+                {
+                    customersQuery = customersQuery.Where(c => c.Address.Suburb == selectedSuburb);
+                }
+            }
+
+            return View(customers);
+        }
+
+        // GET: Customers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers
+                .Include(c => c.Address)
+                .FirstOrDefaultAsync(m => m.CustomerId == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        // GET: Customers/Create
+        public IActionResult Create()
+        {
+            ViewData["AddressId"] = new SelectList(_context.Addresses, "AddressId", "AddressId");
+            return View();
+        }
+
+        // POST: Customers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CustomerId,FirstName,LastName,Email,MainPhoneNumber,SecondaryPhoneNumber,AddressId")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(customer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AddressId"] = new SelectList(_context.Addresses, "AddressId", "AddressId", customer.AddressId);
+            return View(customer);
+        }
+
+        // GET: Customers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            ViewData["AddressId"] = new SelectList(_context.Addresses, "AddressId", "AddressId", customer.AddressId);
+            return View(customer);
+        }
+
+        // POST: Customers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CustomerId,FirstName,LastName,Email,MainPhoneNumber,SecondaryPhoneNumber,AddressId")] Customer customer)
+        {
+            if (id != customer.CustomerId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(customer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CustomerExists(customer.CustomerId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AddressId"] = new SelectList(_context.Addresses, "AddressId", "AddressId", customer.AddressId);
+            return View(customer);
+        }
+
+        // GET: Customers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers
+                .Include(c => c.Address)
+                .FirstOrDefaultAsync(m => m.CustomerId == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer != null)
+            {
+                _context.Customers.Remove(customer);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CustomerExists(int id)
+        {
+            return _context.Customers.Any(e => e.CustomerId == id);
+        }
+    }
+}
