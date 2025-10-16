0a770a7e7defaefc7042606328e9a48f0495a88b
Aaron Bednikof <beday003@mymail.unisa.edu.au>
2025-10-16 15:18:02 +1030

    WK10 - initial commit (completed activity)

--- Controllers/ReportsController.cs
+++ Controllers/ReportsController.cs
@@ -13,33 +13,35 @@ namespace AOWebApp.Controllers
         {
             _context = context;
         }
+
         public IActionResult Index()
         {
-
             var YearList = _context.CustomerOrders.Select(i => i.OrderDate.Year).Distinct().OrderBy(i => i);
-
             return View("AnnualSalesReport", new SelectList(YearList));
         }
 
         [Produces("application/json")]
         public IActionResult AnnualSalesReportData(int? year)
         {
-            if (!year.HasValue || year.Value <= 0) {
+            if (!year.HasValue || year.Value <= 0)
+            {
                 return BadRequest();
             }
 
-            var matches = _context.ItemsInOrders.Where(i => i.OrderNumberNavigation.OrderDate.Year == year)
-            .GroupBy(i => i.OrderNumberNavigation.OrderDate.Month)
-            .Select(i => new
-            {
-                monthName = System.Globalization.CultureInfo.InvariantCulture.DateTimeFormat.GetMonthName(i.Key),
-                Month = i.Key,
-                Year = year,
-                totalItems = i.Sum(i => i.NumberOf),
-                totalSales = i.Sum(i => i.TotalItemCost)
-            }).OrderBy(i => i.Month).AsEnumerable();
-            
-            return Json(matches);
+            var orderSummary = _context.ItemsInOrders
+                .Where(iio => iio.OrderNumberNavigation.OrderDate.Year == year)
+                .GroupBy(iio => new { iio.OrderNumberNavigation.OrderDate.Year, iio.OrderNumberNavigation.OrderDate.Month })
+                .Select(group => new
+                {
+                    year = group.Key.Year,
+                    monthNo = group.Key.Month,
+                    monthName = System.Globalization.CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(group.Key.Month),
+                    totalItems = group.Sum(iio => iio.NumberOf),
+                    totalSales = group.Sum(iio => iio.TotalItemCost)
+                })
+                .OrderBy(data => data.monthNo);
+
+            return Json(orderSummary);
         }
     }
 }
